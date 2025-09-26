1e3ae4e1d0b321ba2ab8d0bb75d649159d6d31b0
ivanpaghubasan <ivan.paghubasan@mph-intl.com>
2025-09-26 15:13:57 +0800

    Adds icon and adds slideshow image type

--- components/images/image-slideshow.tsx
+++ components/images/image-slideshow.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 
 import burgerImg from '@/assets/burger.jpg';
 import curryImg from '@/assets/curry.jpg';
@@ -12,7 +12,12 @@ import schnitzelImg from '@/assets/schnitzel.jpg';
 import tomatoSaladImg from '@/assets/tomato-salad.jpg';
 import classes from './image-slideshow.module.css';
 
-const images = [
+type Slideshow = {
+  image: StaticImageData;
+  alt: string;
+}
+
+const images: Slideshow[] = [
   { image: burgerImg, alt: 'A delicious, juicy burger' },
   { image: curryImg, alt: 'A delicious, spicy curry' },
   { image: dumplingsImg, alt: 'Steamed dumplings' },
